5cda58ed549840b52cbfd604cebabbbab8b8d083
IsabelleSGoff <00001121023836sp@al.educacao.sp.gov.br>
2024-10-24 20:08:08 -0300

    Código - 17/10/2024

---
+++ #17102024.py
@@ -0,0 +1,15 @@
+#17102024
+#Colégio Irene Branco - Téc. em Sistemas
+#Professor: Orlando Gomes
+#Autor: Isabelle S. Goffredo
+#Versão 1.0
+
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+
+soma = v1 + v2
+
+if soma >50:
+    print("A soma é maior que 50.")
+else:
+    print("A soma não é maior que 50.")
